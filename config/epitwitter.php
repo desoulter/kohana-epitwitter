78f8eb2fe8f5f972a854d9614b18d693829559c9
assoulter <assoulter123@gmail.com>
2010-05-19 00:53:14 +0400

    create module for kohana

---
+++ config/epitwitter.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array
+(
+	// put youre consumer keys here
+	'consumer_key'		=> 'utstXsd7JSU7fipEuETfsA',
+        'consumer_secret'       => 'HNh18UNpNz1P3ZHXbEfqnmzIi8Ntiszue78EvGMgk', 
+);
+
---
+++ init.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/* EpiTwitter integration */
+require Kohana::find_file('vendor', 'epitwitter/EpiCurl');
+require Kohana::find_file('vendor', 'epitwitter/EpiOAuth');
+require Kohana::find_file('vendor', 'epitwitter/EpiTwitter');
+
